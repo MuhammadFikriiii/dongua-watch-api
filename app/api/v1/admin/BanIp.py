fa79a17993c3139784907239efce12a86e32c97f
zhadevv <cp.zhadev@gmail.com>
2025-11-18 18:37:39 +0700

    Update BanIp.py

--- app/api/v1/admin/BanIp.py
+++ app/api/v1/admin/BanIp.py
@@ -1,3 +1,19 @@
+#
+#             Zhadevv Project
+#             --MIT License--
+#
+# Feed Me Starnya Bang:>
+# Project 100% Open Source
+# Bebas Recode, Deploy Production. KECUALI
+# Diperjual-Belikan.
+#
+# Project ini Sepenuhnya Gratis, Makannua ksih Bintang Dong anj:>
+# *bercanda ajahh
+#
+# Regards
+# Zhadevv
+#
+
 from fastapi import APIRouter, Query, Request
 from app.api.models.BaseResponse import BaseResponse, ErrorResponse
 from app.api.models.AdminModel import BanIpRequest, BanIpResponse
